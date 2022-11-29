a84657fcd3ad8c5b556a3c16811a62045ae97d45
Christian Rodrigo <chri65mb@stud.kea.dk>
2022-11-29 18:33:27 +0100

    Nyeste, 29.11 19.00

---
+++ fuktioner.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", afspilVideo);
+
+function afspilVideo() {
+  document.querySelector("#play-movie").addEventListener("click", video);
+
+  /*   document.getElementById("play-movie").play; */
+}
+function video() {
+  /*   document.getElementById("play-movie").play();
+/*   const videoPlay = new videoPlay("http://nordiclive.dk/kea/05_indhold/pilot_site/01_politvideo/politvideo.mp4");
+  /*  audiob.volume = 0.9; */
+  /*  videoPlay.play()  */
+}
